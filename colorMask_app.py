9fce92be6007045b5709b61dbe7e6aa57d1de006
FurkanKorlu <furkankorlu46@gmail.com>
2023-08-13 01:21:53 +0300

    Uygulama tabanını oluşturma ve UI test

--- colorMask_app.py
+++ colorMask_app.py
@@ -5,3 +5,18 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QImage, QPixmap
 from colorMask_ui import Ui_ColorMaskApp
+
+class myApp(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(myApp, self).__init__()
+        self.ui = Ui_ColorMaskApp()
+        self.ui.setupUi(self)
+
+def app():
+    app = QtWidgets.QApplication(sys.argv)
+    win = myApp()
+    win.show()
+    sys.exit(app.exec_())
+
+app()
